be7a3b6a3762f43db32fcf13a71dfa78a0ebe988
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2021-02-28 11:13:08 +0200

    mongojs update

---
+++ mongo.js
@@ -0,0 +1,33 @@
+
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `//mongodb+srv://Olumide:${password}>@cluster0.lhdxr.mongodb.net/PhonebookDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
