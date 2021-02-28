9a81134d8d4fcf438f897ab16fb2cec06812f780
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2021-02-28 16:59:52 +0200

    Part3 latest update

--- phonebookbackend/index.js
+++ phonebookbackend/index.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-//const mongoose = require('mongoose')
 const morgan = require ('morgan')
+const Person = require('./models/person')
 
 app.use(express.json())
 
 app.use(express.static('build'))
+
 
 let persons = [
       { 
@@ -44,29 +46,38 @@ app.use(morgan((tokens, req, res) => {
   ].join(' ')
   }))
 
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (request, response) => {
-    response.send(`<h1>Info</h1>
-    <p>Phonebook has information about ${persons.length}people.</p>
-    <p>${new Date()}</p>`)
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({}).then(count => {
+       response.send(`<h4>Phonebook has information for ${count} people</h4> 
+       <p>${new Date()}</p>`)
   })
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+
+
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  .catch(error => next(error))
+})
+
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -76,13 +87,16 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  
-  return Math.floor(Math.random() * 1000000 )
-}
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
  //POST
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
 
@@ -103,16 +117,54 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
-    number: body.number,
-    id: generateId(),
-  }
+    number: body.number 
+  })
+  
+  person.save().then(result => {
+    console.log(`Added ${person.name} with number ${person.number} to phonebook.`)
+  }) 
+
+  .catch(error => next(error))
 
   persons = persons.concat(person)
-
   response.json(person)
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number 
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
  
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- phonebookbackend/mongo.js
+++ phonebookbackend/mongo.js
@@ -9,19 +9,6 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 
-const url =
-  `mongodb+srv://Olumide:${password}@cluster0.lhdxr.mongodb.net/phonebook?retryWrites=true&w=majority`
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
-
-})
-
-const Person = mongoose.model('Person', personSchema)
-
 if (process.argv.length === 5) {
   const name =  process.argv[3]
   const number = process.argv[4]
