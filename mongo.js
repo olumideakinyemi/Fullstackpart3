d589c0ff4b08bd7ee56a103df956edb73e05cc54
Olumide Akinyemi <olumide.akinyemi@centria.fi>
2021-02-28 11:27:00 +0200

    new changes

--- mongo.js
+++ mongo.js
@@ -10,13 +10,13 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `//mongodb+srv://Olumide:${password}>@cluster0.lhdxr.mongodb.net/PhonebookDatabase?retryWrites=true&w=majority`
+  `//mongodb+srv://Olumide:${password}@cluster0.lhdxr.mongodb.net/PhonebookDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
   name: String,
-  number: String,
+  number: String
 
 })
 
@@ -24,7 +24,7 @@ const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
   name: process.argv[3],
-  number: process.argv[4],
+  number: process.argv[4]
 })
 
 person.save().then(result => {
